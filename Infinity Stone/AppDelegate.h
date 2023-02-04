b13cd3777eaa8fd6674340eb2b6c3f06166b8989
hedi <hedihamida98@gmail.com>
2023-02-04 18:56:46 +0100

    Initial Commit

---
+++ Infinity Stone/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Infinity Stone
+//
+//  Created by hedi on 4/2/2023.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Infinity Stone/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Infinity Stone
+//
+//  Created by hedi on 4/2/2023.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
